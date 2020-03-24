8e7b2f5599b0c51b3b7521ddd8df942122aa2585
fearo <fearo@JF-Home-Desktop>
2020-03-24 07:45:32 +0000

    Funtion Added to Frame Work for PriceDataClass, Deleted depricated docs

--- Prog2-CW2/src/PriceDataClass.java
+++ Prog2-CW2/src/PriceDataClass.java
@@ -1,11 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 /**
  * @author 24248134
- *
+ * @version 0.1
  */
 //import java.util.HashMap;
 
 public class PriceDataClass 
 {
+	// HashMap KG-£ values
+	private static HashMap<String, String> RM1L = new HashMap<String, String>(); // Royal Mail 1st Letter
+	private static HashMap<String, String> RM1SP = new HashMap<String, String>(); // Royal Mail 1st Small Parcel
+	private static HashMap<String, String> RM1MP = new HashMap<String, String>(); // Royal Mail 1st Medium Parcel
+	private static HashMap<String, String> RM2L = new HashMap<String, String>(); // Royal Mail 2nd Letter
+	private static HashMap<String, String> RM2SP = new HashMap<String, String>(); // Royal Mail 2nd Small Parcel
+	private static HashMap<String, String> RM2MP = new HashMap<String, String>(); // Royal Mail 2nd Medium Parcel
+	private static HashMap<String, String> RM1LS = new HashMap<String, String>(); // Royal Mail 1st Letter Signed
+	private static HashMap<String, String> RM1SPS = new HashMap<String, String>(); // Royal Mail 1st Small Parcel Signed
+	private static HashMap<String, String> RM1MPS = new HashMap<String, String>(); // Royal Mail 1st Medium Parcel Signed
+	private static HashMap<String, String> RM2LS = new HashMap<String, String>(); // Royal Mail 2nd Letter Signed
+	private static HashMap<String, String> RM2SPS = new HashMap<String, String>(); // Royal Mail 2nd Small Parcel Signed
+	private static HashMap<String, String> RM2MPS = new HashMap<String, String>(); // Royal Mail 2nd Medium Parcel Signed
 	/**
 	 * @param GetShipCost Calls the method that links to ServiceID[0] then returns the value from that method
 	 * @return Shipping-cost
@@ -39,16 +58,6 @@ public class PriceDataClass
 		// TODO Create Table
 		return 0;
 	}
-	private double RMT48(int kg, int i)
-	{
-		// TODO Create Table
-		return 0;
-	}
-	private double RMT24(int kg, int i) 
-	{
-		// TODO Create Table
-		return 0;
-	}
 	private double RM2(int kg, int i) 
 	{
 		// TODO Create Table
@@ -59,4 +68,34 @@ public class PriceDataClass
 		// TODO Create Table
 		return 0;
 	}
+	/**
+	 * @param updateCSV updates the CSV ratio table for a given sheet
+	 */
+	public static void updateCSV() // reads file and returns contents as a string
+	{
+		BufferedReader in = null;
+		String[] PathNames = {"RM1L","RM1LS","RM1MP","RM1MPS","RM1SP","RM1SPS","RM2L","RM2LS","RM2MP","RM2MPS","RM2SP","RM2SPS"};
+		String Path = "C:\\Users\\fearo\\git\\Prog2-CW2\\Prog2-CW2\\src\\CSV";
+		for (int i = 0; i != PathNames.length; i++) 
+		{
+			try
+			{
+				in = new BufferedReader(new FileReader((Path + PathNames[i])));
+				try
+				{
+					String line = "";	// Creates an empty string
+					while ((line = in.readLine()) != null) // makes line equal to the next line of the file until next line is null
+					{
+						String[] x = line.split(",");				
+						PathNames[i].put(x[0],x[1]);
+					}
+				}
+				catch(IOException e) {}
+				finally{in.close();}
+			}
+			catch (FileNotFoundException e) { }
+			catch (IOException e) { }	
+		}
+	}
+
 }
--- Prog2-CW2/src/testesttest.java
+++ Prog2-CW2/src/testesttest.java
@@ -1,15 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class testesttest 
 {
+	private static HashMap<String, String> RM1L = new HashMap<String, String>(); // Royal Mail 1st Letter
+	
 	public static void main(String[] args) 
-	{
-		HashMap<String, String> RM1st = new HashMap<String, String>();
-	    
-	    
-	    for (String i : RM1st.keySet()) {
-	      System.out.println("key: " + i + " value: " + RM1st.get(i));
-	    }
+	{    
+	    String path = "C:\\Users\\fearo\\Documents\\test\\RM1-L.csv";
+	    BufferedReader in = null;
+		try
+		{
+			in = new BufferedReader(new FileReader(path));
+			try
+			{
+				String H = in.readLine();
+				String line = "";	// Creates an empty string
+				while ((line = in.readLine()) != null) // makes line equal to the next line of the file until next line is null
+				{
+					String[] x = line.split(",");				
+					RM1L.put(x[0],x[1]);
+				}
+			}
+			
+			// IDK
+			catch(IOException e)
+			{
+				// Error while reading the file...
+			}
+			// Close the file stream when finished reading it.
+			finally{in.close();}
+		}
+		catch (FileNotFoundException e) 
+		{
+			// File could not be found on the disc.
+		}
+		catch (IOException e)
+		{
+			//Could not open or close file.
+		}	
+		System.out.println(RM1L);
 	}
 
 }
