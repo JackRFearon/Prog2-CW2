f38c27c98302edd8f18ab6e04abee5178644aebc
JackRFearon <fearonjj22@gmail.com>
2020-03-24 21:53:19 +0000

    Commenting Cleaning in PriceDataClass

--- Prog2-CW2/src/PriceDataClass.java
+++ Prog2-CW2/src/PriceDataClass.java
@@ -6,25 +6,27 @@ import java.util.HashMap;
 
 /**
  * @author 24248134
- * @version 0.1
+ * @version 0.2
  */
-//import java.util.HashMap;
-
 public class PriceDataClass 
 {
 	// HashMap KG-£ values
-	private static HashMap<String, String> RM1L = new HashMap<String, String>(); // Royal Mail 1st Letter
-	private static HashMap<String, String> RM1SP = new HashMap<String, String>(); // Royal Mail 1st Small Parcel
-	private static HashMap<String, String> RM1MP = new HashMap<String, String>(); // Royal Mail 1st Medium Parcel
-	private static HashMap<String, String> RM2L = new HashMap<String, String>(); // Royal Mail 2nd Letter
-	private static HashMap<String, String> RM2SP = new HashMap<String, String>(); // Royal Mail 2nd Small Parcel
-	private static HashMap<String, String> RM2MP = new HashMap<String, String>(); // Royal Mail 2nd Medium Parcel
-	private static HashMap<String, String> RM1LS = new HashMap<String, String>(); // Royal Mail 1st Letter Signed
-	private static HashMap<String, String> RM1SPS = new HashMap<String, String>(); // Royal Mail 1st Small Parcel Signed
-	private static HashMap<String, String> RM1MPS = new HashMap<String, String>(); // Royal Mail 1st Medium Parcel Signed
-	private static HashMap<String, String> RM2LS = new HashMap<String, String>(); // Royal Mail 2nd Letter Signed
-	private static HashMap<String, String> RM2SPS = new HashMap<String, String>(); // Royal Mail 2nd Small Parcel Signed
-	private static HashMap<String, String> RM2MPS = new HashMap<String, String>(); // Royal Mail 2nd Medium Parcel Signed
+	// Royal Mail 1st
+	private static HashMap<String, String> RM1L = new HashMap<String, String>(); // Letter
+	private static HashMap<String, String> RM1SP = new HashMap<String, String>(); // Small Parcel
+	private static HashMap<String, String> RM1MP = new HashMap<String, String>(); // Medium Parcel
+	// Royal Mail 2nd
+	private static HashMap<String, String> RM2L = new HashMap<String, String>(); // Letter
+	private static HashMap<String, String> RM2SP = new HashMap<String, String>(); // Small Parcel
+	private static HashMap<String, String> RM2MP = new HashMap<String, String>(); // Medium Parcel
+	// Royal Mail 1st Signed
+	private static HashMap<String, String> RM1LS = new HashMap<String, String>(); // Letter Signed
+	private static HashMap<String, String> RM1SPS = new HashMap<String, String>(); // Small Parcel Signed
+	private static HashMap<String, String> RM1MPS = new HashMap<String, String>(); // Medium Parcel Signed
+	// Royal Mail 2nd Signed
+	private static HashMap<String, String> RM2LS = new HashMap<String, String>(); // Letter Signed
+	private static HashMap<String, String> RM2SPS = new HashMap<String, String>(); // Small Parcel Signed
+	private static HashMap<String, String> RM2MPS = new HashMap<String, String>(); // Medium Parcel Signed
 	/**
 	 * @param GetShipCost Calls the method that links to ServiceID[0] then returns the value from that method
 	 * @return Shipping-cost
@@ -38,40 +40,68 @@ public class PriceDataClass
 		case 2:
 			return RM2(kg,ServiceID[1]);
 		case 3:
-			return RMT24(kg,ServiceID[1]);
-		case 4:
-			return RMT48(kg,ServiceID[1]);
-		case 5:
 			return RM1S(kg,ServiceID[1]);
-		case 6:
+		case 4:
 			return RM2S(kg,ServiceID[1]);
 		}
 		return 999.999; // Should not reach this >_<
 	}
-	private double RM2S(int kg, int i) 
+	private double RM2S(int kg, int i) // ServiceID { 4 , i } Royal Mail 2nd Signed...
 	{
-		// TODO Create Table
-		return 0;
+		switch(i) 
+		{
+		case 1: // ServiceID { 4 , 1 } Royal Mail Signed - Letter
+			return Double.parseDouble(RM2LS.get(kg));
+		case 2: // ServiceID { 4 , 2 } Royal Mail Signed - Small Parcel
+			return Double.parseDouble(RM2SPS.get(kg));
+		case 3: // ServiceID { 4 , 3 } Royal Mail Signed - Medium Parcel
+			return Double.parseDouble(RM2MPS.get(kg));
+		}
+		return 999.999; // Should not reach this >_<
 	}
-	private double RM1S(int kg, int i)
+	private double RM1S(int kg, int i) // ServiceID { 3 , i } Royal Mail 1st Signed..
 	{
-		// TODO Create Table
-		return 0;
+		switch(i) 
+		{
+		case 1: // ServiceID { 3 , 1 } Royal Mail 1st Signed - Letter
+			return Double.parseDouble(RM1LS.get(kg));
+		case 2: // ServiceID { 3 , 2 } Royal Mail 1st Signed - Small Parcel
+			return Double.parseDouble(RM1SPS.get(kg));
+		case 3: // ServiceID { 3 , 3 } Royal Mail 1st Signed - Medium Parcel
+			return Double.parseDouble(RM1MPS.get(kg));
+		}
+		return 999.999; // Should not reach this >_<
 	}
-	private double RM2(int kg, int i) 
+	private double RM2(int kg, int i) // ServiceID { 2 , i } Royal Mail 2nd...
 	{
-		// TODO Create Table
-		return 0;
+		switch(i) 
+		{
+		case 1: // ServiceID { 3 , 1 } Royal Mail 2nd - Letter
+			return Double.parseDouble(RM2L.get(kg));
+		case 2: // ServiceID { 3 , 2 } Royal Mail 2nd - Small Parcel
+			return Double.parseDouble(RM2SP.get(kg));
+		case 3: // ServiceID { 3 , 3 } Royal Mail 2nd - Medium Parcel
+			return Double.parseDouble(RM2MP.get(kg));
+		}
+		return 999.999; // Should not reach this >_<
 	}
-	private double RM1(int kg, int i) 
+	private double RM1(int kg, int i) // ServiceID { 1 , i } Royal Mail 1st...
 	{
-		// TODO Create Table
-		return 0;
+		switch(i) 
+		{
+		case 1: // ServiceID { 3 , 1 } Royal Mail 1st - Letter
+			return Double.parseDouble(RM1L.get(kg));
+		case 2: // ServiceID { 3 , 2 } Royal Mail 1st - Small Parcel
+			return Double.parseDouble(RM1SP.get(kg));
+		case 3: // ServiceID { 3 , 3 } Royal Mail 1st - Medium Parcel
+			return Double.parseDouble(RM1MP.get(kg));
+		}
+		return 999.999; // Should not reach this >_<
 	}
 	/**
 	 * @param updateCSV updates the CSV ratio table for a given sheet
 	 */
-	public static void updateCSV() // reads file and returns contents as a string
+	public static void updateCSV() 
 	{
 		BufferedReader in = null;
 		String[] PathNames = {"RM1L","RM1LS","RM1MP","RM1MPS","RM1SP","RM1SPS","RM2L","RM2LS","RM2MP","RM2MPS","RM2SP","RM2SPS"};
@@ -86,8 +116,22 @@ public class PriceDataClass
 					String line = "";	// Creates an empty string
 					while ((line = in.readLine()) != null) // makes line equal to the next line of the file until next line is null
 					{
-						String[] x = line.split(",");				
-						PathNames[i].put(x[0],x[1]);
+						String[] x = line.split(",");
+						switch(i)
+						{
+						 case 1: RM1L.put(x[0],x[1]); break;
+						 case 2: RM1LS.put(x[0],x[1]); break;
+						 case 3: RM1MP.put(x[0],x[1]); break;
+						 case 4: RM1MPS.put(x[0],x[1]); break;
+						 case 5: RM1SP.put(x[0],x[1]); break;
+						 case 6: RM1SPS.put(x[0],x[1]); break;
+						 case 7: RM2L.put(x[0],x[1]); break;
+						 case 8: RM2LS.put(x[0],x[1]); break;
+						 case 9: RM2MP.put(x[0],x[1]); break;
+						 case 10: RM2MPS.put(x[0],x[1]); break;
+						 case 11: RM2SP.put(x[0],x[1]); break;
+						 case 12: RM2SPS.put(x[0],x[1]); break;
+						}		
 					}
 				}
 				catch(IOException e) {}
@@ -97,5 +141,4 @@ public class PriceDataClass
 			catch (IOException e) { }	
 		}
 	}
-
 }
--- Prog2-CW2/src/testesttest.java
+++ Prog2-CW2/src/testesttest.java
@@ -1,48 +1,14 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-
 public class testesttest 
-{
-	private static HashMap<String, String> RM1L = new HashMap<String, String>(); // Royal Mail 1st Letter
-	
+{	
 	public static void main(String[] args) 
-	{    
-	    String path = "C:\\Users\\fearo\\Documents\\test\\RM1-L.csv";
-	    BufferedReader in = null;
-		try
-		{
-			in = new BufferedReader(new FileReader(path));
-			try
-			{
-				String H = in.readLine();
-				String line = "";	// Creates an empty string
-				while ((line = in.readLine()) != null) // makes line equal to the next line of the file until next line is null
-				{
-					String[] x = line.split(",");				
-					RM1L.put(x[0],x[1]);
-				}
-			}
-			
-			// IDK
-			catch(IOException e)
-			{
-				// Error while reading the file...
-			}
-			// Close the file stream when finished reading it.
-			finally{in.close();}
-		}
-		catch (FileNotFoundException e) 
-		{
-			// File could not be found on the disc.
-		}
-		catch (IOException e)
-		{
-			//Could not open or close file.
-		}	
-		System.out.println(RM1L);
+	{   
+		System.out.println("aa");
+		int x = 0;
+	    for (int i = 0; i != 10; i++) 
+	    {
+	    	x++;
+	    	System.out.println(x);
+	    }
 	}
 
 }
